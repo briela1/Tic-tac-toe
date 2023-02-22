2d365eb631f85991a5505c4018347448730ecf6b
briela1 <98447320+briela1@users.noreply.github.com>
2023-02-21 19:20:28 -0500

    winner

--- src/App.jsx
+++ src/App.jsx
@@ -20,22 +20,64 @@ const Square = ({children, updateBoard, index, isSelected}) =>{
     </div>
   )
 }
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 function App() {
   const [board, setBoard] = useState(
     Array(9).fill(null)
   )
   const [turn, setTurn] = useState(TURNS.X)
+    // null es que no hay ganador, false es que hay un empate
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+      // revisamos todas las combinaciones ganadoras para ver si X u O ganó
+      for (const combo of WINNER_COMBOS) {
+        const [a, b, c] = combo  //e.g. [0, 3, 6]
+        if (
+          boardToCheck[a] && // posición 0 -> x u o
+          boardToCheck[a] === boardToCheck[b] && //0 y 3 -> x->x  u o->o  Tienen lo mismo?
+          boardToCheck[a] === boardToCheck[c] // 0 y 6  son iguales -> 3 en raya
+        ) {
+          return boardToCheck[a] //x u o, el ganador
+        }
+      }
+      // si no hay ganador
+      return null
+    }
 
   const updateBoard = (index) => {
-    // no actualizamos esta posición si ya tiene algo
-    if (board[index]) return
+    // no actualizamos esta posición si ya tiene algo O si hay un ganador ya
+    if (board[index] || winner) return 
     // actualizar el tablero
     const newBoard = [...board] //evita modificar original
     newBoard[index] = turn
-    setBoard(newBoard)
+    setBoard(newBoard) //Es ASINCRONA
     // cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // revisar si hay ganador
+    const newWinner = checkWinner(newBoard)//Por parametro le paso el valor correcto, último tablero. Solución a asincronismo. 
+    if (newWinner) {
+      
+      setWinner(newWinner)
+      // setWinner((prevWinner) => {
+      //   console.log(`Ganador: ${newWinner}, el anterior era ${prevWinner}`)
+      //   return newWinner
+      // })
+      //Tienes acceso al valor anterior, pero no puedes hacer un async await
+      //no devuelve una promesa
+    }
   }
 
   return (
